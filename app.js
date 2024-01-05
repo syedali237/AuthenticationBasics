35471eeec22dca883756468aa78fc00f854eb289
syedali237 <syedaliulhasan19@gmail.com>
2024-01-06 02:55:40 +0530

    Hashing using MD5

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 //jshint esversion:6
-import 'dotenv/config';
+import "dotenv/config";
 import express from "express";
 import bodyParser from "body-parser";
 import mongoose, { mongo } from "mongoose";
 import encrypt from "mongoose-encryption";
+import md5 from "md5";
 import { profile } from "console";
 
 const app = express();
@@ -17,72 +18,77 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 //mongoDB
-mongoose.connect("mongodb://localhost:27017/userDB")
-.then(()=> console.log('Connected to DB'));
+mongoose
+  .connect("mongodb://localhost:27017/userDB")
+  .then(() => console.log("Connected to DB"));
 
 const userSchema = new mongoose.Schema({
-    email : String,
-    password : String
-})
+  email: String,
+  password: String,
+});
+
+//using mongoose-encryption
 
 // using secret string instead if two keys for convenience
-const secret = process.env.SECRET;
-userSchema.plugin(encrypt, { secret: secret , excludeFromEncryption: ["email"]}); 
+
+// const secret = process.env.SECRET;
+// userSchema.plugin(encrypt, { secret: secret , excludeFromEncryption: ["email"]}); 
+
 // for multiples [email , ___]
 // before mongoose model
 
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
 
-app.get("/" , (req,res)=>{
-    res.render("home");
+app.get("/", (req, res) => {
+  res.render("home");
 });
 
-app.get("/login" , (req,res)=>{
-    res.render("login");
+app.get("/login", (req, res) => {
+  res.render("login");
 });
 
-app.get("/register" , (req,res)=>{
-    res.render("register");
+app.get("/register", (req, res) => {
+  res.render("register");
 });
 
-app.post("/register" , (req,res)=>{
+app.post("/register", (req, res) => {
+  const newUser = new User({
+    email: req.body.username,
+    password: md5(req.body.password),
+  });
 
-     const newUser = new User({
-         email : req.body.username ,
-         password: req.body.password
-     });
+  newUser
+    .save()
+    .then(() => {
+      res.render("secrets");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
-     newUser.save()
-     .then(() => {
-        res.render("secrets");
-     })
-     .catch((err)=>{
-        console.log(err);
-     })
-})
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  const password = md5(req.body.password);
 
-app.post("/login" , (req,res)=>{
-    const username =req.body.username;
-    const password = req.body.password;
-
-    User.findOne({email : username})
-    .then(foundUser => {
-        if (foundUser) {
-            console.log('Found User', foundUser.email);
-            if (foundUser.password === password){
-                res.render("secrets");
-            }
-        } else {
-          console.log('User not found');
+  User.findOne({ email: username })
+    .then((foundUser) => {
+      if (foundUser) {
+        console.log("Found User", foundUser.email);
+        if (foundUser.password === password) {
+          res.render("secrets");
         }
-      })
-      .catch(error => {
-        console.log(error);
-      });
-})
+      } else {
+        console.log("User not found");
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
 
-app.listen(port , () => {
-    console.log(`App Running on port : ${port}`);
+app.listen(port, () => {
+  console.log(`App Running on port : ${port}`);
 });
